46306e070cc7a90840b7cb8aa73e1a83ddbba0c7
Luciano Júnior <luciannojunior@gmail.com>
2020-01-02 19:55:40 -0300

    Adding JSON parsing (the working kind)

--- Program.fs
+++ Program.fs
@@ -1,8 +1,44 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.IO
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Runtime.Serialization
 
-open System
+let rec qsort: int list -> int list= function
+    | [] -> []
+    | x::xs -> let smaller = [for a in xs do if a<=x then yield a]
+               let larger =  [for b in xs do if b>x then yield b]
+               qsort smaller @ [x] @ qsort larger
+
+let readFile(path: string) = File.ReadAllText path
+
+[<DataContract>]
+type Input =
+   { 
+      [<field: DataMember(Name = "list")>]
+      list: int seq;
+   }
+ 
+type Output =
+   { 
+      [<field: DataMember(Name = "result")>]
+      result : int seq;
+   }
+
+let sortHandler: Input -> Output = fun i -> { result= [1;2] }
+
+let result = { result= [1;2;3] }
+
+let app = 
+  choose
+    [ POST >=> choose
+        [path "/" >=> Json.mapJson sortHandler]]
+
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    startWebServer defaultConfig app
+    0
